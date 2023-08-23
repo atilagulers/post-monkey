0a94001fa71ba3d63405c13c2eefb137449283ac
atilagulers <atilagulerofficial@gmail.com>
2023-08-23 21:24:05 +0300

    -

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import {register} from './controllers/auth';
 
 //* CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -18,6 +19,45 @@ app.use(express.urlencoded({limit: '30mb', extended: true}));
 app.use(express.json({limit: '30mb', extended: true}));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
-app.use(morgon('common'));
+app.use(morgan('common'));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+//* FILE STORAGE
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    const originalName = file.originalname;
+    const extname = path.extname(originalName);
+    const fileNameWithoutExt = path.basename(originalName, extname);
+    const uniqueFileName = `${fileNameWithoutExt}-${Date.now()}${extname}`;
+    cb(null, uniqueFileName);
+  },
+});
+
+const upload = multer({storage});
+
+//* ROUTES WITH FILES
+app.post('/auth/register', upload.single('picture'), register);
+
+//* MONGOOSE SETUP
+const PORT = process.env.PORT | 3000;
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    await app.listen(PORT);
+
+    console.log(`Server is listening on port ${PORT}`);
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
+}
+
+startServer();
