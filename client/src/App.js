7feb59c7942ec925ca04b7468d30080e1b57522f
atilagulers <atilagulerofficial@gmail.com>
2023-08-24 23:55:00 +0300

    -

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
+
+// Pages
+import Home from 'Pages/Home';
+import Login from 'Pages/Login';
+import Profile from 'Pages/Profile';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
