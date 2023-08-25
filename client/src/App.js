ef25794cad33bf299ac891bfd35140b06aab3455
atilagulers <atilagulerofficial@gmail.com>
2023-08-25 19:29:35 +0300

    -

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,26 @@ import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
 import Home from 'Pages/Home';
 import Login from 'Pages/Login';
 import Profile from 'Pages/Profile';
+import {useMemo} from 'react';
+import {useSelector} from 'react-redux/es/hooks/useSelector';
+import {CssBaseline, ThemeProvider, createTheme} from '@mui/material';
+import {themeSettings} from './theme';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
